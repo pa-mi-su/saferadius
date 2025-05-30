8fcebe60aa6cbad32ec6e415a67a83f0d1c23a32
pa-mi-su <pamisu@protonmail.com>
2025-05-30 10:38:01 -0400

    updated all services docker files and main classes and props

---
+++ crime-service/src/main/java/com/crimeapp/crimeservice/CrimeServiceApplication.java
@@ -0,0 +1,11 @@
+package com.crimeapp.crimeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrimeServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CrimeServiceApplication.class, args);
+    }
+}
